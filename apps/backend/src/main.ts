f0f151569d54fe706a881f603e2866a57141a8af
klpineda <klpa2704@gmail.com>
2026-08-20 17:06:32 +0200

    feat(auth): enable secure cookie parsing

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
 
 import { AppModule } from "./app.module";
 
@@ -7,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix("api");
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
